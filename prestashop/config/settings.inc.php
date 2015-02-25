ae2d3f34165f5a6113b88bd266f2e7b5ed12a4ce
Muhammad Anis <muhammad.anis@veritrans.co.id>
2015-02-26 00:05:41 +0700

    add prestashop

---
+++ prestashop/config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'presta');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'eHEbC3Fh1yAS8NEtE1fKDDThW8C5rBBLRJEjP9Ju25XZXPoOyMmy0dEG');
+define('_COOKIE_IV_', 'SnkIutNz');
+define('_PS_CREATION_DATE_', '2015-02-25');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.13');
+define('_RIJNDAEL_KEY_', 'XCvt2JsZIaB7bik8CZBjx2f98eZSkXFW');
+define('_RIJNDAEL_IV_', 'wmKXH0Ym6nE+yt3Wjgs09A==');
